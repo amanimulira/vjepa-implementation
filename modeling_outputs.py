6308231090161d6cdd78bea001c8b82da9ff4b11
amanimulira <amanimulira@gmail.com>
2025-07-08 14:27:15 +0100

    modeling outputs

---
+++ modeling_outputs.py
@@ -0,0 +1,22 @@
+import warnings
+from dataclasses import dataclass
+from typing import Optional
+
+import torch
+
+from .utils import ModelOutput
+
+@dataclass 
+class BaseModelOutput(ModelOutput):
+
+    last_hidden_state: Optional[torch.FloatTensor] = None
+    hidden_states: Optional[tuple[torch.FloatTensor, ...]] = None
+    attentions: Optional[tuple[torch.FloatTensor, ...]] = None
+
+@dataclass
+class ImageClassifierOutput(ModelOutput):
+
+    loss: Optional[torch.FloatTensor] = None
+    logits: Optional[torch.FloatTensor] = None
+    hidden_states: Optional[tuple[torch.FloatTensor, ...]] = None
+    attentions: Optional[tuple[torch.FloatTensor, ...]] = None
